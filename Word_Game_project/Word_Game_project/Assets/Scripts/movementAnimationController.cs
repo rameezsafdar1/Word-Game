3c8a7cf12d2dfd2301742a400c45840dd04f912e
RamizMakesGames <57453901+rameezsafdar1@users.noreply.github.com>
2022-10-30 13:05:47 +0500

    Updates

---
+++ Word_Game_project/Word_Game_project/Assets/Scripts/movementAnimationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementAnimationController : MonoBehaviour
+{
+    public Animator anim;
+    public CharacterController controller;   
+    
+
+    private void Update()
+    {
+        anim.SetFloat("Velocity", controller.velocity.magnitude);
+    }
+
+}
